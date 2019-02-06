acff05bf939304174a9286ca96bf832bc153f29f
Yann Lucas <yannjdl@gmail.com>
2019-02-06 17:11:09 -0300

    First class: <Position>

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Chess now!");
+		Position p = new Position(4,8);
+		System.out.println(p);
 
 	}
 
