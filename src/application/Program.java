b335504182742d492f82976500ea4946178d7c2e
Yann Lucas <yannjdl@gmail.com>
2019-02-06 17:36:58 -0300

    Starting to implement <Board> and <Piece>

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,9 @@
 package application;
 
-import boardgame.Position;
-
 public class Program {
 
 	public static void main(String[] args) {
-		Position p = new Position(4,8);
-		System.out.println(p);
+		
 
 	}
 
